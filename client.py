a52bef31b8294d218dbadf5087b1a2c7938dfab8
victor4correa <v.correar@alumnos.urjc.es>
2018-10-23 13:51:38 +0200

    Modificado client.py para meter datos desde linea de comandos

--- client.py
+++ client.py
@@ -3,13 +3,13 @@
 """
 Programa cliente UDP que abre un socket a un servidor
 """
-
+import sys
 import socket
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = 'localhost'
-PORT = 6001
-LINE = '¡Hola mundo!'
+SERVER = sys.argv[1]
+PORT = int(sys.argv[2])
+LINE = " ".join(sys.argv[3:])
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:

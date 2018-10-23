3ca9aa39bf58cf8e098433179e7ad8ebe84858ca
victor4correa <v.correar@alumnos.urjc.es>
2018-10-23 14:06:31 +0200

    server.py imprime direccion y puerto cliente y lee su puerto en shell

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 """
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
-
+import sys
 import socketserver
 
 
@@ -17,14 +17,17 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         handle method of the server class
         (all requests will be handled by this method)
         """
+        print(self.client_address)
         self.wfile.write(b"Hemos recibido tu peticion")
         for line in self.rfile:
             print("El cliente nos manda ", line.decode('utf-8'))
 
+
 if __name__ == "__main__":
-    # Listens at localhost ('') port 6001 
+    # Listens at localhost ('') port 6001
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', 6001), EchoHandler) 
+    PORT = int(sys.argv[1])
+    serv = socketserver.UDPServer(('', PORT), EchoHandler)
 
     print("Lanzando servidor UDP de eco...")
     try:
